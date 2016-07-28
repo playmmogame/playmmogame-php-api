c33184b30725d722d29ecae2396d220fb4ab85b6
Steven Liu <Steven@Zylinkus.com>
2016-07-28 19:52:11 +0800

    update the user login and auth function on the login it

--- Playmmogame.php
+++ Playmmogame.php
@@ -6,26 +6,26 @@
 */
 class Playmmogame {
 	public $gametoken; // game token
-	public $uid; 
+	public $user; 
 	public $access_token; //when player login,the server will give a oauth token to client,and player put it to server for access token;
 	const AUTH_URL = "https://playmmogame.com/v1/auth";
-	const USER_URL = "https://playmmogame.com/v1/member/info";
-	public function __construct($uid,$game_token) {
+	const USER_URL = "https://playmmogame.com/v1/user";
+	public function __construct($user,$game_token) {
 		$this->game_token = $game_token;
-		$this->uid = $uid;
-		$this->playmmogame($uid,$game_token);
+		$this->user = $user;
+		$this->playmmogame($user,$game_token);
 	}
 	
-	public function playmmogame($uid,$game_token) {
+	public function playmmogame($user,$game_token) {
 		$this->game_token = $game_token;
-		$this->uid = $uid;
-		$this->auth($uid,$game_token);
+		$this->user = $user;
+		$this->auth($user,$game_token);
 	}
 	
 	public function auth() {
 		//you can config it cache file for get the access info
-		$array['uid'] = $this->uid;
-		$array['game_token'] = $this->game_token;
+		$array['user'] = $this->user;
+		$array['token'] = $this->game_token;
 		$access_token = $this->request();
 		$this->access_token = $access_token;
 		return $this->access_token;
--- Test.php
+++ Test.php
@@ -1,7 +1,7 @@
 <?php
 require_once ("Playmmogame.php");
-$uid = trim($_GET['uid']);
+$user = trim($_GET['user']);
 $game_token = ""; //got it from game developer center https://playmmogame.com/developer
-$client = new Playmmogame($uid, $game_token);
+$client = new Playmmogame($user, $game_token);
 $access_token = $client->auth(); //get access token
 $member = $client->member();
