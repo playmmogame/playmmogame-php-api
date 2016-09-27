0a156387653bf4edd2bfa48bd2efde81aeb532cb
Steven Liu <Steven@Zylinkus.com>
2016-09-27 18:04:12 +0800

    Add API TEST Images

--- Test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once ("Playmmogame.php");
-$user = trim($_GET['user']);
-$game_token = ""; //got it from game developer center https://playmmogame.com/developer
-$client = new Playmmogame($user, $game_token);
-$access_token = $client->auth(); //get access token
-$member = $client->member();
